edaf7f29a480933a104ce364f4468de712af2f6b
Jean-Marc Epis <jeanmarc.epis@gmail.com>
2021-08-25 15:22:09 +0200

    Added possibility to see all models using buttons

---
+++ modelPicker.js
@@ -0,0 +1,23 @@
+const models = [
+  "models/BATEAU_LAVOIR.glb",
+  "models/EGLISE.glb",
+  "models/MAIRIE.glb"
+];
+
+const viewer = document.getElementById("viewer");
+const container = document.getElementById("buttonblock");
+let i;
+models.map(function(e) {
+  const newButton = document.createElement("button");
+  container.appendChild(newButton);
+  newButton.innerHTML = `${e}`;
+  container.style.position = "absolute";
+  container.style.top = "0";
+  container.style.left = "0"
+  newButton.addEventListener("click", function() {
+
+    viewer.src = e;
+    console.log("Hi");
+    console.log(e);
+  });
+});
