66f3aac35ee028de9d88adec36bfe94d449130f8
Jean-Marc Epis <jeanmarc.epis@gmail.com>
2021-08-27 10:19:20 +0200

    Added the new models and made the buttons text more user friendly

--- modelPicker.js
+++ modelPicker.js
@@ -1,24 +1,50 @@
 const models = [
-  "models/BATEAU_LAVOIR.glb",
-  "models/EGLISE.glb",
-  "models/MAIRIE.glb",
-  "models/PONT_1.glb",
-  "models/PONT_2.glb",
-  "models/PONT_3.glb",
-  "models/REMPART.glb"
+  {
+    model : "models/BATEAU_LAVOIR/BATEAU_LAVOIR.glb",
+    nom: "Bateau Lavoir"
+  },
+
+  {
+    model : "models/EGLISE/EGLISE.glb",
+    nom: "Eglise"
+  },
+
+  {
+    model : "models/MAIRIE/MAIRIE.glb",
+    nom: "Mairie"
+  },
+
+  {
+    model : "models/PONT_1882/PONT_1882.glb",
+    nom: "Pont 1882"
+  },
+
+  {
+    model : "models/PONT_1923/PONT_1923.glb",
+    nom: "Pont 1923"
+  },
+  
+  {
+    model : "models/PONT_PROVISOIRE/PONT_PROVISOIRE.glb",
+    nom: "Pont Provisoire"
+  },
+
+  {
+    model : "models/REMPARTS/REMPARTS.glb",
+    nom: "Remparts"
+  }
 ];
 
 const viewer = document.getElementById("viewer");
 const container = document.getElementById("buttonblock");
-let i;
 models.map(function(e) {
   const newButton = document.createElement("button");
   container.appendChild(newButton);
-  newButton.innerHTML = `${e}`;
+  newButton.innerHTML = `${e.nom}`;
   container.style.position = "absolute";
   container.style.top = "0";
   container.style.left = "0"
   newButton.addEventListener("click", function() {
-    viewer.src = e;
+    viewer.src = e.model;
   });
 });
